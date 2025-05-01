173396725b2897f39150c769022fd70acf7e87d4
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2025-05-01 20:47:13 +0300

    [context] Supress several types at once

--- misclib/context.py
+++ misclib/context.py
@@ -7,38 +7,49 @@ class Supress:
     A context manager for suppressing specific exception types.
 
     >>> from misclib.context import Supress
-    >>> result = 1
-    >>> result
-    1
-    >>> with Supress(ZeroDivisionError):
-    ...     error = 1 / 0
-    ...     result = 10
-    ...
-    >>> result
-    1
-    >>> with Supress(ValueError):
-    ...     error = 1 / 0
-    ...     result = 10
-    ...
+    >>> 1 / 0
     Traceback (most recent call last):
         ...
     ZeroDivisionError: division by zero
+    >>> with Supress(ZeroDivisionError):
+    ...     1 / 0
+    ...
 
     The supression is also applied for subclasses of the specified exception type:
 
     >>> issubclass(ZeroDivisionError, ArithmeticError)
     True
     >>> with Supress(ArithmeticError):
-    ...     error = 1 / 0
-    ...     result = 10
+    ...     1 / 0
     ...
-    >>> result
-    1
-    """
-    __slots__ = '_exc_cls',
 
-    def __init__(self, exception_type: type[BaseException], /) -> None:
-        self._exc_cls = exception_type
+    The manager does not supress errors that are not a subtype of the specified one:
+
+    >>> with Supress(ValueError):
+    ...     1 / 0
+    ...
+    Traceback (most recent call last):
+        ...
+    ZeroDivisionError: division by zero
+
+    To supress several exceptions types, pass them to the constructor:
+
+    >>> with Supress(ZeroDivisionError, ValueError):
+    ...     1 / 0
+    ...
+    """
+    __slots__ = '_error_classes',
+
+    def __init__(self, exc_type: type[BaseException], /, *exc_types: type[BaseException]) -> None:
+        exception_types = exc_type, *exc_types
+        for i, exc_type in enumerate(exception_types):
+            if not (isinstance(exc_type, type) and issubclass(exc_type, BaseException)):
+                raise TypeError(
+                    f'expected a subclass of {BaseException.__name__}, '
+                    f'got {exc_type!r} at position {i}'
+                    )
+
+        self._error_classes = exception_types
 
     def __enter__(self, /) -> Self:
         return self
@@ -50,7 +61,7 @@ class Supress:
             exc_tb: TracebackType | None,
             /,
             ) -> bool:
-        return exc_type and issubclass(exc_type, self._exc_cls)
+        return exc_type and issubclass(exc_type, self._error_classes)
 
 
 __all__ = 'Supress',
